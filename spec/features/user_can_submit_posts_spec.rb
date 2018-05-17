bf015ea4effe7c68c600bf045c04da84f424d832
Marwa Elali <marwaelali@gmail.com>
2018-05-17 16:45:19 +0100

    adding tests and spec helpers

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,11 +1,71 @@
 require 'rails_helper'
+require_relative './spec_helper/user_signup'
+require_relative './spec_helper/create_post'
 
 RSpec.feature "Timeline", type: :feature do
-  # scenario "Can submit posts and view them" do
-  #   visit "/posts"
-  #   click_link "New post"
-  #   fill_in "Message", with: "Hello, world!"
-  #   click_button "Submit"
-  #   expect(page).to have_content("Hello, world!")
-  # end
+  scenario "User can sign up" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "user_username", with: "test1"
+    fill_in "user_email", with: "test1@gmail.com"
+    fill_in "user_password", with: "test-1"
+    fill_in "user_password_confirmation", with: "test-1"
+    click_button "Sign up"
+    expect(page).to have_content("test1's Wall")
+  end
+
+  scenario "User can sign in" do
+    signup
+    click_link "Logout"
+    click_link "Log in"
+    fill_in "user_email", with: "test1@gmail.com"
+    fill_in "user_password", with: "test-1"
+    click_button "Log in"
+    expect(page).to have_content("test1's Wall")
+  end
+
+  scenario "User can submit posts and view them" do
+    signup
+    create_post
+    expect(page).to have_content("Hello, world!")
+  end
+
+  scenario "User can edit posts" do
+    signup
+    create_post
+    click_link "Edit"
+    fill_in "post_message", with: "Howdy"
+    click_button "Update Post"
+    expect(page).to have_content("Howdy")
+  end
+
+  scenario "User can delete posts" do
+    signup
+    create_post
+    click_link "Delete"
+    # click_button "OK"
+    expect(page).to_not have_content("Hello, world!")
+  end
+
+  scenario "Posts have a like feature" do
+    signup
+    create_post
+    expect(page).to have_content("Likes: 0")
+  end
+
+  scenario "User can like a post" do
+    signup
+    create_post
+    click_link "Like"
+    expect(page).to have_content("Likes: 1")
+  end
+
+  scenario "User can comment on posts" do
+    signup
+    create_post
+    fill_in "comment_content", with: "Awesome"
+    find("form input[id=comment_content]").set "\n"
+
+    expect(page).to have_content("comment: Awesome")
+  end
 end
