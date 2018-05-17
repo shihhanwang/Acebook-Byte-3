85d2049cde78a9b9b88d8242403213224e64254e
Irbe Krumina <irbekrm@gmail.com>
2018-05-17 07:27:30 +0100

    user can add likes to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,20 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!
 
+  def add_like
+    @like = Like.new()
+    @like.post_id = params['post_id']
+    @like.user_id = current_user.id
+    @like.save
+    redirect_to posts_url
+  end
+
+  def show_likes
+    puts 'Showing likes..'
+    puts params
+    Like.count.select { |like| like.post_id == params['post_id'] }
+  end
+
   def new
     @post = current_user.posts.new
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
 
   validates :user_id, presence: true
   belongs_to :user
+  has_many :likes
 
 end
